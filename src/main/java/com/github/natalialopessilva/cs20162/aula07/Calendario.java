56380ff365a79f9a03c4969a86cef369c6289f43
NataliaLopesSilva <natalialopescig@gmail.com>
2016-10-19 19:33:34 -0200

    Adição de nota de Copyrigth

--- src/main/java/com/github/natalialopessilva/cs20162/aula07/Calendario.java
+++ src/main/java/com/github/natalialopessilva/cs20162/aula07/Calendario.java
@@ -1,6 +1,8 @@
 /*
- * 2016 - Instituto de Informática (UFG)
+ * Copyright (c) 2016. Fábrica de Software - Instituto de Informática (UFG)
+ * Creative Commons Attribution 4.0 International License.
  */
+
 package com.github.natalialopessilva.cs20162.aula07;
 
 /**
