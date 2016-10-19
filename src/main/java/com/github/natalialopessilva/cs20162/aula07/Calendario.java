8addb54094440180b425ac817d33b22e7ee33c75
Natalia <natalialopescig@gmail.com>
2016-10-19 14:37:49 -0200

    Projeto final com as ultimas atualizações e correções realizadas.

--- src/main/java/com/github/natalialopessilva/cs20162/aula07/Calendario.java
+++ src/main/java/com/github/natalialopessilva/cs20162/aula07/Calendario.java
@@ -33,8 +33,8 @@ public final class Calendario {
     }
 
     /**
-    * Acessa o construtor protegido
-    */
+     * Acessa o construtor protegido.
+     */
     public static void setCalendario() {
         Calendario meuConstrutor = new Calendario();
     }
